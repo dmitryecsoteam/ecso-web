2dd53b2d834f18211f41f5b275c873b5f19d7293
Dmitry Kiselev <dmitry.ecsoteam@gmail.com>
2019-03-06 18:59:57 +0300

    Add test for search form action

---
+++ src/tests/actions/searchFormActions.test.js
@@ -0,0 +1,20 @@
+import { setSearchForm } from '../../actions/searchFormActions';
+
+test('should setup search form action object', () => {
+
+    const form = {
+        originInputValue: 'Tokyo',
+        originsSelectedId: 3,
+        destinationInputValue: 'Osaka',
+        destinationSelectedId: 5,
+        date: '2019-01-01',
+        parametersPanel: false,
+        parametersValue: [0, 0, 0, 0]
+    }
+    const action = setSearchForm(form);
+
+    expect(action).toEqual({
+        type: 'SET_SEARCH_FORM',
+        ...form
+    });
+});
