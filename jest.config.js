c8ab6841d1907e1f7ecde397fdd443849363bdfa
Dmitry Kiselev <dmitry.ecsoteam@gmail.com>
2019-04-16 20:36:44 +0300

    Jest and Enzyme configuration. Header component tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     "setupFiles": [
         "<rootDir>/src/tests/setupTests.js"
-    ]
+    ],
+    "moduleNameMapper": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMock.js"
+    },
+    "snapshotSerializers": ["enzyme-to-json/serializer"]
 }
---
+++ src/tests/components/Header/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../../components/Header';
+
+test('should render Header without title prop', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render Header with title prop', () => {
+    const wrapper = shallow(<Header title="Moscow, Russia" />);
+    expect(wrapper).toMatchSnapshot();
+})
