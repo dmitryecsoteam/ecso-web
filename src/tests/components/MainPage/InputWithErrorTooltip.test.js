c27efd17673c0f47e0aeb54f1827b3f2cc2b5235
Dmitry Kiselev <dmitry.ecsoteam@gmail.com>
2019-04-18 01:21:43 +0300

    InputWithErrorTooltip tests

---
+++ src/tests/components/MainPage/InputWithErrorTooltip.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import InputWithErrorTooltip from '../../../components/MainPage/InputWithErrorTooltip';
+
+test('should render InputWithErrorTooltip component with input and default props', () => {
+    const wrapper = shallow(<InputWithErrorTooltip>
+        <input type="text" />
+    </InputWithErrorTooltip>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render InputWithErrorTooltip component with input', () => {
+    const wrapper = shallow(<InputWithErrorTooltip
+        error={false}
+        errorText=""
+        label="Test"
+        disabled={false}
+    >
+        <input type="text" />
+    </InputWithErrorTooltip>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render InputWithErrorTooltip disabled', () => {
+    const wrapper = shallow(<InputWithErrorTooltip
+        error={false}
+        errorText=""
+        label="Test"
+        disabled={true}
+    >
+        <input type="text" />
+    </InputWithErrorTooltip>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render InputWithErrorTooltip with error message', () => {
+    const wrapper = shallow(<InputWithErrorTooltip
+        error={true}
+        errorText="Please type text"
+        label="Test"
+        disabled={true}
+    >
+        <input type="text" />
+    </InputWithErrorTooltip>);
+    expect(wrapper).toMatchSnapshot();
+});
