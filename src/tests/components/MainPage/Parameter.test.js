02848202e61d42d1b794a02d1f76169b5dc8f93e
Dmitry Kiselev <dmitry.ecsoteam@gmail.com>
2019-04-18 15:29:59 +0300

    Refactor Parameter tests

--- src/tests/components/MainPage/Parameter.test.js
+++ src/tests/components/MainPage/Parameter.test.js
@@ -2,29 +2,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Parameter from '../../../components/MainPage/Parameter';
 
+let wrapper, onChange;
 
-const testProps = {
-    parameter: "Test parameter",
-    value: 2,
-    onChange: () => {},
-    disabled: false
-};
+beforeEach(() => {
+    onChange = jest.fn();
+    wrapper = shallow(<Parameter
+        parameter="Test parameter"
+        value={2}
+        onChange={onChange}
+        disabled={false}
+    />);
+});
 
 test('should render Parameter component with default props', () => {
-    const wrapper = shallow(<Parameter {...testProps} />);
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should pass new value in onChange function on direct input change', () => {
-    const onChange = jest.fn(); 
-    const wrapper = shallow(<Parameter {...{...testProps, onChange}} />);
-    wrapper.find('input').simulate('change', { target: { value: 4 }});
+    wrapper.find('input').simulate('change', { target: { value: 4 } });
     expect(onChange).toBeCalledWith(4, "Test parameter");
 });
 
 test('should pass new value in onChange function on label click', () => {
-    const onChange = jest.fn(); 
-    const wrapper = shallow(<Parameter {...{...testProps, onChange}} />);
-    wrapper.find('.slider__label').at(0).simulate('click', { target: { innerHTML: "0" }});
+    wrapper.find('.slider__label').at(0).simulate('click', { target: { innerHTML: "0" } });
     expect(onChange).toBeCalledWith(0, "Test parameter");
 });
