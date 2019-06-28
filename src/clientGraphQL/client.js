1e2dfb194505eb8ef65532d3b204acaab191c487
Dmitry Kiselev <dmitry.ecsoteam@gmail.com>
2019-06-28 21:08:26 +0300

    Version 0.0.1 ready for production

--- src/clientGraphQL/client.js
+++ src/clientGraphQL/client.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import history from '../router/history';
 
 export const client = new ApolloClient({
-    uri: "http://localhost:4000/v1",
+    uri: "http://194.182.70.179:4000/v1",
     onError: ({ graphQLErrors, networkError }) => {
       if (networkError || graphQLErrors) {
         //console.log(graphQLErrors);
